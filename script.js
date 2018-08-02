01d705a4a160bd04c33cd8814e5319302ecf94cb
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-03 00:08:25 +1000

    Premitive data type

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello world");
+var firstName = 'Deepender';
+console.log(firstName);
+
+var secondName = 'Rathore';
+var age = 27;
+
+var fullAge = true;
+console.log(fullAge);
+
+var job;
+console.log(job);
+
+
+job = 'Teacher';
+console.log(job);
+
+var _3years = 3;
+var $4years = 4;
