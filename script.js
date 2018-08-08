50df5f1ca9f9642e66bcb865d02b7f76382f7104
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-08 21:11:14 +1000

    Operator precedence & Accosiativity

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
 /**
- * Basic Operators
+ *  Operators precedence
+ * 
+ *  Refernece : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#Table
  */
 
-//Math operators
-var now, yearJohn,yearMark;
-now = 2018;
-ageJohn = 28;
-ageMark = 33;
-yearJohn = now - 28;
-yearMark = now - 33; 
-console.log(yearJohn);
-console.log(yearMark);
+ var now = 2018;
+ var yearJohhn = 1989;
+ var fullAge = 18;
 
-console.log(now + 2)
-console.log(now * 2)
-console.log(now / 2)
+ //Multiple operators
+ var isFullAge = now - yearJohhn > fullAge; //true
+ console.log(isFullAge);
+ 
+ //Grouping
+ var ageJohn = now - yearJohhn;
+ var ageMark = 35;
+ var avgAge = (ageJohn + ageMark)/2;
+ console.log(avgAge);
+ 
+//Multiple assignment
+var x,y;
+x = y = (3 + 5) * 6 + 4;// 8 * 6 + 4 //48 + 4 //52
+console.log(x,y); // associativity
+
+//More operator
+x *= 4; // 52 * 4 = 208
+console.log(x);
+
+x += 10; // 208 + 10 = 218
+console.log(x);
+
+x++;
+console.log(x);//219
 
 
-//Logical operators
-var johnOlder = ageJohn < ageMark;
-console.log(johnOlder);
 
 
-//typeof operator
-console.log(typeof johnOlder);
-console.log(typeof ageJohn);
-console.log(typeof "My name is deep");
-
-var x;
-console.log(typeof x);
-  
+ 
