ead5e4966a9a4c1a550537c90e130f70d2a31df6
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-11 21:39:12 +1000

    BMI calculation

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-/**
- *  Operators precedence
- * 
- *  Refernece : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#Table
- */
+/*****************************
+* CODING CHALLENGE 1
+*/
 
- var now = 2018;
- var yearJohhn = 1989;
- var fullAge = 18;
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
+4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
 
- //Multiple operators
- var isFullAge = now - yearJohhn > fullAge; //true
- console.log(isFullAge);
- 
- //Grouping
- var ageJohn = now - yearJohhn;
- var ageMark = 35;
- var avgAge = (ageJohn + ageMark)/2;
- console.log(avgAge);
- 
-//Multiple assignment
-var x,y;
-x = y = (3 + 5) * 6 + 4;// 8 * 6 + 4 //48 + 4 //52
-console.log(x,y); // associativity
-
-//More operator
-x *= 4; // 52 * 4 = 208
-console.log(x);
-
-x += 10; // 208 + 10 = 218
-console.log(x);
-
-x++;
-console.log(x);//219
+*/
+var markMassInKg = prompt("Enter Mark weigh in Kg:"); // 80KG
+var johnMassInKg = prompt("Enter John weight in Kg:"); // 100KG
 
 
+var markHeightInMeter = prompt("Enter Mark height in meter:"); //1.6
+var johnHeightInMeter = prompt("Enter John height in meter:"); //1.95
 
 
- 
+var markBMI = markMassInKg / (markHeightInMeter * markHeightInMeter); // 29.3847566575
+var johnBMI = johnMassInKg / (johnHeightInMeter * johnHeightInMeter); //26.2984878369
+
+var isMarkBMIHigher = markBMI > johnBMI;
+
+console.log("Mark BMI is " + markBMI +" and John BMI is " + johnBMI +". Mark BMI is heigher than John: "+isMarkBMIHigher);
