1e5540ad573d08c1c2c74ce1b04cb12572b560c8
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-18 15:49:45 +1000

    empty value in variable

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 //Falsy: undefined, null, 0 , '', NaN
 //Truthy: all other values
 
-var height = 0; // Since 0 is falsy value the loop will still go to not defined
+var height = ''; // Since 0 is falsy value the loop will still go to not defined
 
 if(height || height === 0){
     console.log('Height has been defined');
