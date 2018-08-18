89d73e53fa8b5968e7be15fba55e5512842468bc
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-18 14:04:02 +1000

    boolean logic

--- script.js
+++ script.js
@@ -1,31 +1,20 @@
 /*****************************
-* CODING CHALLENGE 1
+* Boolean login
 */
 
-/*
-Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
-1. Store Mark's and John's mass and height in variables
-2. Calculate both their BMIs
-3. Create a boolean variable containing information about whether Mark has a higher BMI than John.
-4. Print a string to the console containing the variable from step 3. (Something like "Is Mark's BMI higher than John's? true"). 
+var firstName = "Deep";
+var age = 31;
 
-*/
-var markMassInKg = prompt("Enter Mark weigh in Kg:"); // 80KG
-var markHeightInMeter = prompt("Enter Mark height in meter:"); //1.6
-
-
-var johnMassInKg = prompt("Enter John weight in Kg:"); // 100KG
-var johnHeightInMeter = prompt("Enter John height in meter:"); //1.95
-
-var markBMI = markMassInKg / (markHeightInMeter * markHeightInMeter); // 29.3847566575
-var johnBMI = johnMassInKg / (johnHeightInMeter * johnHeightInMeter); //26.2984878369
-
-
-var isMarkBMIHigher = markBMI > johnBMI;
-
-if (markBMI > johnBMI) {
-    console.log('Mark\'s BMI is greater than John\'s');
+if(age < 12){
+    console.log('John is a boy');
+    
+}else if (age >= 12 && age <20){
+    console.log('John is a teenage');
+    
+}else if (age >= 20 && age <=30){
+    console.log('John is a young man');
     
 }else{
-    console.log('John\'s BMI is greater than Mark\'s');
+    console.log('John is a man');
+    
 }
