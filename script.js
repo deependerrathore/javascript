5dd6962b2c0c2423b1796959a0391702156c8b5d
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-20 23:54:55 +1000

    Calculate avg without draw condition

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 /*****************************
-* Truthy and Falsy values and equality operator
+* CODING CHALLENGE 2
 */
 
-//Falsy: undefined, null, 0 , '', NaN
-//Truthy: all other values
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
 
-var height = 23; // Since 0 is falsy value the loop will still go to not defined
+*/
+var scoreJohn = (89+120+103)/3;
+var scoreMike = (116+94+123)/3;
 
-if(height || height === 0){
-    console.log('Height has been defined');
-    
-}else{
-    console.log('Height has NOT been defined');
-    
-}
-
-
-//equality operator
-if(height === '23'){
-    console.log('the == operator does the type coercion!');
-}else{
-    console.log('the === opetrator does not do the type coercion');
-    
-}
+scoreJohn > scoreMike ? console.log('John avg is more than Mike' + scoreJohn) : console.log('Mike avg is more than John: '+ scoreMike);
