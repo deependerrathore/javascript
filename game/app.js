2d25e3b2c9cc6a8b9bc3172c1c20c9909b4d4264
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-22 00:39:24 +1000

    btn-roll implimented

--- game/app.js
+++ game/app.js
@@ -9,15 +9,15 @@ GAME RULES:
 
 */
 
-var scores, roundScore,activePlayer,dice;
+var scores, roundScore,activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;//setting this up as 0 so that we can acess scores array easly
 
-dice = Math.floor((Math.random() * 6 )) + 1;
 
-document.querySelector('#current-'+activePlayer).textContent = dice;
+
+//document.querySelector('#current-'+activePlayer).textContent = dice;
 //document.querySelector('#current-'+activePlayer).innerHTML = '<em>'+dice+'</em>'; // if we want to set html
 
 var x = document.querySelector('#score-'+activePlayer).textContent;
@@ -27,6 +27,13 @@ console.log(x);
 document.querySelector('.dice').style.display = 'none';
 
 
-document.querySelector('.btn-roll').addEventListener('click',btn(){
-    //TODO
-}); //anonymous function
+document.querySelector('.btn-roll').addEventListener('click',function(){
+    // 1. Random number
+    var dice = Math.floor((Math.random() * 6 )) + 1;
+
+    // 2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+    //3. Update the round score IF the rolled number was NOT a 1
+}); 
