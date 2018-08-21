93fe9919787221b91c0b0a282b5cc41f364c4169
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-21 22:39:27 +1000

    method borrowing - this keyword

--- script.js
+++ script.js
@@ -1,42 +1,19 @@
 /*
-* HOISTING 
-Learning: Hoisting will only work for function declaration not expression
- */
-
-
- /////////////////////////////////
- //Function
- ////////////////////////////////
-//calculateAge(1965); //this is stored in global context before execution
-//and during execution will call this
-
-function calculateAge(year) {
-    console.log(2018 - year);
-    
+* Borrowing method - THIS is invoked when method is getting called
+*/
+var john = {
+    firstname: 'deepak',
+    yearBirth: 1992,
+    calAge: function(){
+        console.log(2018 - this.yearBirth);
+    }
 }
 
-//calculateAge(1991);
-
-
-//retirement(1990);//this will not work and will give us a error that function retirement does not exist
-var retirement = function(year){
-    console.log(65 - (2018 - year));
+var mark = {
+    firstname: 'mark',
+    yearBirth: 1960
 }
 
-//retirement(1990);
+mark.calAge = john.calAge;
 
-/////////////////////////////////
-//VARIABLES
-/////////////////////////////////
-
-//console.log(age); // will give us undefined but if we remove the below line than than it will give us n error 
-var age = 23;
-console.log(age);
-
-function foo(){
-    console.log(age);//undefined
-    var age = 65;
-    console.log(age);
-}
-foo();
-console.log(age);
+mark.calAge();
