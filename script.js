9a8ea6516317ea45c4c6b43c6b3022d06e6691ca
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-21 10:10:55 +1000

    FUNCTIONS - DRY

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-/*****************************
-* CODING CHALLENGE 2
-*/
-
 /*
-John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
-1. Calculate the average score for each team
-2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
-3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
-4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
-5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
-
+* Functions
 */
-var scoreJohn = (89+120+103)/3;
-var scoreMike = (116+94+123)/3;
-var scoreMary = (97+131+105)/3;
-console.log(scoreJohn,scoreMike,scoreMary);
 
-if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
-    console.log('John\'s wins the match with ' + scoreJohn + ' points');
-    
-}else if(scoreMike > scoreJohn && scoreMike > scoreMary){
-    console.log('Mike\'s wins the match with ' + scoreJohn + ' points');
-}else if( scoreMary > scoreJohn && scoreMary > scoreMike){
-    console.log('Mary\'s wins the match with ' + scoreJohn + ' points');
-    
-}else{
-    console.log('the match is draw');
+function calculateAge(birthyear) {
+    return 2018 - birthyear;
 }
+
+var ageJohn = calculateAge(1992);
+var ageMark = calculateAge(1950);
+var ageJane = calculateAge(1999);
+
+console.log(ageJohn , ageMark,ageJane);
+
+function ageUntilRetirement(firstname,year) {
+    var age = calculateAge(year);
+    var retirement = 65 - age;
+    if (retirement > 0) {
+        console.log(firstname + ' retires in ' + retirement + ' years.');
+    } else {
+        console.log(firstname + ' is already retired');
+    }
+    
+}
+
+ageUntilRetirement('john',1992);
+ageUntilRetirement('mark',1950);
+ageUntilRetirement('jane',1999);
