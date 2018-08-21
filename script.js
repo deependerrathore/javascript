54d306e8986f4275d443c9499afa60735acc072f
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-21 13:02:45 +1000

    this keyword in object

--- script.js
+++ script.js
@@ -6,15 +6,16 @@
 var john = {
     firstName: 'john',
     lastName: 'smith',
-    birthYear: 1991,
+    birthYear: 1955,
     familyMember: ['jane', 'mark','ted'],
     isMarried: false,
     job: 'teacher',
     calcAge: function(){
-        return 2018 - this.birthYear;
+        this.age =  2018 - this.birthYear;
     }
 };
 
 console.log(john.calcAge());
 
-john.age = john.calcAge(); // setting up the age property
+john.calcAge(); 
+console.log(john);
