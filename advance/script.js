1ca5bf56578e85a901b705068fd0c5cad52c6c81
Deepender Rathore <deepenrathore28@gmail.com>
2018-08-31 14:29:46 +1000

    Object.create"

--- advance/script.js
+++ advance/script.js
@@ -1,5 +1,6 @@
 //Function constructor
 
+/*
 var john = {
     name: 'john',
     yearOfBirth: 1990,
@@ -31,3 +32,25 @@ console.log(john.lastName);
 console.log(mark.lastName);
 console.log(jane.lastName);
 
+*/
+
+//Object.create
+
+var personProto = {
+    calculateAge: function() {
+        console.log(2018 - this.yearOfBirth);
+        
+    }
+};
+
+var john = Object.create(personProto);
+
+john.firstName = 'john';
+john.yearOfBirth = 1992;
+john.job = 'Teacher';
+
+var jane = Object.create(personProto,{
+    name: {value: 'jane'},
+    yearOfBirth: {value: 1980},
+    job: {value: 'Designer'}
+})
