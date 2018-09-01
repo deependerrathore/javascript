adffc89987a4f5da30ce8de31eba309e59651dd8
Deepender Rathore <deepenrathore28@gmail.com>
2018-09-02 04:40:57 +1000

    adding new item to the list

--- budget/app.js
+++ budget/app.js
@@ -5,34 +5,34 @@ var budgetController = (function(){
         this.description = description;
         this.value = value;
     };
-
+    
     var Income = function(id,description,value){
         this.id = id;
         this.description = description;
         this.value = value;
     };
     
-  
-
+    
+    
     var data = {
-         allItems:{
-             exp:[],
-             inc:[]
-         },
-         total:{
-             exp:0,
-             inc:0
-         }
+        allItems:{
+            exp:[],
+            inc:[]
+        },
+        total:{
+            exp:0,
+            inc:0
+        }
     };
-
+    
     return {
         addItem:function(type,desc,val){
             var newItem,ID;
-
+            
             //create new ID
             if(data.allItems[type].length > 0){
                 ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
-
+                
             }else{
                 ID = 0;
             }
@@ -46,11 +46,11 @@ var budgetController = (function(){
             data.allItems[type].push(newItem);
             return newItem;
         },
-
+        
         testing: function(){
             console.log(data);
         }
-
+        
         
     };
 })();
@@ -62,7 +62,9 @@ var UIController = (function(){
         inputType:'.add__type',
         inputDescription:'.add__description',
         inputValue: '.add__value',
-        inputAddBtn: '.add__btn'
+        inputAddBtn: '.add__btn',
+        incomeContainer: '.income__list',
+        expensesContainer:'.expenses__list'
     }
     return{
         getinput:function(){
@@ -73,51 +75,71 @@ var UIController = (function(){
             }
             
         },
-        getDOMStrings: function(){
-            return DOMStrings;
-        }
-    };
-})();
-
-
-//GLOBAL APP CONTROLLER
-var controller = (function(budgetCtrl,UICtrl){
-    
-    var setupEventListeners = function(){
-
-        var DOM = UICtrl.getDOMStrings();
-
-        document.querySelector(DOM.inputAddBtn).addEventListener('click',ctrlAddItem);
-        
-        document.addEventListener('keypress',function(event){
-            if(event.keyCode===13 || event.which === 13){
-                ctrlAddItem();            
+        addListItem:function(obj,type){
+            var html,newHtml,element;
+            //Create HTML string with placeholder text
+            if(type ==='inc'){
+                element = DOMStrings.incomeContainer;
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
-        });
-    };
-
-
-    var ctrlAddItem = function(){
-        var input , newItem;
-        // 1. Get the field input data
-        input = UICtrl.getinput();
-     
-        // 2. Add the item to the budget controller
-        newItem = budgetCtrl.addItem(input.type,input.description,input.value);
-        // 3. Add the item to the UI 
-        // 4. Calculate the budget
-        // 5. Display the budget on the UI
+            else if(type === 'exp'){
+                element = DOMStrings.expensesContainer;
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+                //Replace the placeholder text with acutal data
+                newHtml = html.replace('%id%',obj.id);
+                newHtml = newHtml.replace('%description%',obj.description);
+                newHtml = newHtml.replace('%value%',obj.value);
+                //Insert the HTML into to DOM
+                document.querySelector(element).insertAdjacentHTML('beforeend',newHtml);
+            },
+            getDOMStrings: function(){
+                return DOMStrings;
+            }
+        };
+    })();
+    
+    
+    //GLOBAL APP CONTROLLER
+    var controller = (function(budgetCtrl,UICtrl){
+        
+        var setupEventListeners = function(){
+            
+            var DOM = UICtrl.getDOMStrings();
+            
+            document.querySelector(DOM.inputAddBtn).addEventListener('click',ctrlAddItem);
+            
+            document.addEventListener('keypress',function(event){
+                if(event.keyCode===13 || event.which === 13){
+                    ctrlAddItem();            
+                }
+            });
+        };
         
         
-    };
+        var ctrlAddItem = function(){
+            var input , newItem;
+            // 1. Get the field input data
+            input = UICtrl.getinput();
+            
+            // 2. Add the item to the budget controller
+            newItem = budgetCtrl.addItem(input.type,input.description,input.value);
+            
+            // 3. Add the item to the UI 
+            UICtrl.addListItem(newItem,input.type);
+            // 4. Calculate the budget
+            // 5. Display the budget on the UI
+            
+            
+        };
+        
+        return{
+            init: function(){
+                console.log('Application started');
+                setupEventListeners();
+            }
+        };
+        
+    })(budgetController,UIController);
     
-    return{
-        init: function(){
-            console.log('Application started');
-            setupEventListeners();
-        }
-    };
-    
-})(budgetController,UIController);
-
-controller.init();
+    controller.init();
