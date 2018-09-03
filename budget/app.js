1154999c37eaccb4ae12bfd0e17663383af27022
Deepender Rathore <deepenrathore28@gmail.com>
2018-09-03 23:57:53 +1000

    updating the percentages: Controller

--- budget/app.js
+++ budget/app.js
@@ -204,7 +204,15 @@ var controller = (function(budgetCtrl,UICtrl){
         UICtrl.displayBudget(budget);
         
         
-    }
+    };
+
+    var updatePercentages = function(){
+        // 1. Calculate percentages
+
+        // 2. Read percentages from the budget controller
+
+        // 3. Update the UI with the new percentages
+    };
     var ctrlAddItem = function(){
         var input , newItem;
         // 1. Get the field input data
@@ -223,6 +231,9 @@ var controller = (function(budgetCtrl,UICtrl){
             
             // 5. Calculate and update budget
             updateBudget();
+
+            // 6. Calculate and update percentages
+            updatePercentages();
         }
         
     };
@@ -243,6 +254,10 @@ var controller = (function(budgetCtrl,UICtrl){
             UICtrl.deleteListItem(itemID);
             // 3. Update and show the new budget
             updateBudget();
+
+            
+            // 4. Calculate and update percentages
+            updatePercentages();
         }
     };
     return{
